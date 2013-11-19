a5a3f54201981348db97065a02bfe3b255db26c6
Benjamín Hernández <benjamin.hernandez@in2teck.com>
2013-11-19 11:31:54 -0600

    Included new schema with url_thumb for gallery_images

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20131118223336) do
+ActiveRecord::Schema.define(:version => 20131119170100) do
 
   create_table "ads", :force => true do |t|
     t.string   "image_url"
@@ -51,8 +51,9 @@ ActiveRecord::Schema.define(:version => 20131118223336) do
     t.string   "image_url"
     t.integer  "gallery_id"
     t.boolean  "visible"
-    t.datetime "created_at",  :null => false
-    t.datetime "updated_at",  :null => false
+    t.datetime "created_at",      :null => false
+    t.datetime "updated_at",      :null => false
+    t.string   "image_url_thumb"
   end
 
   create_table "items", :force => true do |t|
